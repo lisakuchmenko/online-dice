8142a59f7f6e8892570dcaf3c2977d2267200073
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2022-05-16 13:56:22 +0200

    added main functionality(roll)

--- src/RollDice.js
+++ src/RollDice.js
@@ -2,17 +2,36 @@ import { Component } from 'react';
 import Die from './Die';
 import './RollDice.css';
 
+const dieFace = {
+	1: 'one',
+	2: 'two',
+	3: 'three',
+	4: 'four',
+	5: 'five',
+	6: 'six',
+};
+
 class RollDice extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { die1: 'one', die2: 'five' };
+		this.roll = this.roll.bind(this);
 	}
+
+	roll() {
+		const rand1 = dieFace[Math.floor(Math.random() * 6 + 1)];
+		const rand2 = dieFace[Math.floor(Math.random() * 6 + 1)];
+		this.setState({ die1: rand1, die2: rand2 });
+	}
+
 	render() {
 		return (
 			<div className='RollDice'>
 				<Die die={this.state.die1} />
 				<Die die={this.state.die2} />
-				<button type='button'>Roll Dice!</button>
+				<button type='button' onClick={this.roll}>
+					Roll Dice!
+				</button>
 			</div>
 		);
 	}

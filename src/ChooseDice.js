7bd3f0638785998ccd2f95bd7faa3dc9aa94b9e1
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2022-05-17 10:40:33 +0200

    implemented ability to choose die number

--- src/ChooseDice.js
+++ src/ChooseDice.js
@@ -5,19 +5,28 @@ import './ChooseDice.css';
 class ChooseDice extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			selectedValue: 1,
+		};
+		this.handleDropdown = this.handleDropdown.bind(this);
+	}
+
+	handleDropdown(e) {
+		this.setState({ selectedValue: Number(e.target.value) });
 	}
 	render() {
 		return (
 			<div className='chooseDice'>
 				<label htmlFor='die-num'>Choose a number of dice:</label>
-				<select name='die-num' id='die-num'>
+				<select name='die-num' id='die-num' onChange={this.handleDropdown}>
+					<option value='0'></option>
 					<option value='1'>1</option>
 					<option value='2'>2</option>
 					<option value='3'>3</option>
 					<option value='4'>4</option>
 					<option value='5'>5</option>
 				</select>
-				<RollDice count={5} />
+				<RollDice count={this.state.selectedValue} />
 			</div>
 		);
 	}
